ac088ed2f9ac2784392aa2bf3198d3f4bf734344
Adesiyan Divine <divinegiftadesiyan@gmail.com>
2024-11-28 11:39:12 +0100

    implement post controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,15 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateBlogPost;
+use App\Models\Blog;
 use App\Models\Post;
+use App\Traits\Responses;
 
 class PostController extends Controller
 {
+    use Responses;
 
-//     getBlogPosts
-// createBlogPost
-// getSinglePost
-// updateSinglePost
-// deleteSinglePost
+    public function getBlogPosts(Blog $blog)
+    {
+        return $this->sendSuccess("Blog Posts Fetched Successfully", $blog->posts()->paginate(30));
+    }
+
+    public function createBlogPost(Blog $blog, CreateBlogPost $request)
+    {
+
+        $imagePath = $request->file('cover_image')->store("post_image", 'public');
+
+        $post = $blog->posts()->create([
+            "post_title" => $request->title,
+            "post_cover_image" => $imagePath,
+            "post_content" => $request->content
+        ]);
+
+        return $this->sendCreated("Blog Post Created Successfully", $post);
+    }
+
+    public function getSinglePost(Post $post)
+    {
+        return $this->sendSuccess("Post Created Successfully", $post);
+    }
+
+    public function updateSinglePost(Post $post, CreateBlogPost $request)
+    {
+
+        $post->update([
+            "post_title" => $request->title,
+            "post_content" => $request->content
+        ]);
+
+        return $this->sendSuccess("Post Updated Successfully", $post);
+    }
+
+    public function deleteSinglePost(Post $post)
+    {
+
+        $post->delete();
+        return $this->sendSuccess("Post Deleted Successfully");
+    }
 
 }
--- app/Http/Requests/CreateBlogPost.php
+++ app/Http/Requests/CreateBlogPost.php
@@ -11,7 +11,7 @@ class CreateBlogPost extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class CreateBlogPost extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "title" => "required|string",
+            "content" => "required|string",
+            "cover_image" => "sometimes|nullable|image"
         ];
     }
 }
